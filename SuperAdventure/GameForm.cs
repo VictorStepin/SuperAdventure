a91caa4ffbecb444692672a59ee4079771d3f85b
Victor Stepin <vicstep1994@gmail.com>
2022-02-26 12:37:19 +0700

    MoveTo function temporary moved to GameForm class

--- SuperAdventure/GameForm.cs
+++ SuperAdventure/GameForm.cs
@@ -12,8 +12,8 @@ namespace SuperAdventure
             InitializeComponent();
 
             _player = new Player(100, 100, 0, 0, 1);
-            _player.RequiredItemNotify += message => rtbMessages.Text += $"{message}\n";
-            _player.MoveTo(World.LocationByID(World.LOCATION_ID_HOME));
+            
+            MoveTo(World.LocationByID(World.LOCATION_ID_HOME));
 
             UpdateUI();
         }
@@ -33,27 +33,72 @@ namespace SuperAdventure
             btnWest.Enabled = _player.CurrentLocation.LocationToWest != null ? true : false;
         }
 
+        public void MoveTo(Location locationToMove)
+        {
+            if (locationToMove.ItemRequiredToEnter != null)
+            {
+                bool playerHaveRequiredItem = false;
+                foreach (var ii in _player.Inventory)
+                {
+                    if (ii.Details.ID == locationToMove.ItemRequiredToEnter.ID)
+                    {
+                        playerHaveRequiredItem = true;
+                        break;
+                    }
+                }
+
+                if (!playerHaveRequiredItem)
+                {
+                    rtbMessages.Text += $"Для прохода в \"{locationToMove.Name}\" необходим предмет: {locationToMove.ItemRequiredToEnter.Name}";
+                    return;
+                }
+            }
+
+            _player.CurrentLocation = locationToMove;
+
+            _player.CurrentHitPoints = _player.MaximumHitPoints;
+
+            if (locationToMove.QuestAvailableHere != null)
+            {
+                bool playerAlreadyHasQuest = false;
+                foreach (var pq in _player.Quests)
+                {
+                    if (pq.Details.ID == locationToMove.QuestAvailableHere.ID)
+                    {
+                        playerAlreadyHasQuest = true;
+                        break;
+                    }
+                }
+
+                if (!playerAlreadyHasQuest)
+                {
+                    _player.Quests.Add(new PlayerQuest(locationToMove.QuestAvailableHere));
+                    // Добавление квеста в грид квестов в интерфейс
+                }
+            }
+        }
+
         private void btnNorth_Click(object sender, System.EventArgs e)
         {
-            _player.MoveTo(_player.CurrentLocation.LocationToNorth);
+            MoveTo(_player.CurrentLocation.LocationToNorth);
             UpdateUI();
         }
 
         private void btnEast_Click(object sender, System.EventArgs e)
         {
-            _player.MoveTo(_player.CurrentLocation.LocationToEast);
+            MoveTo(_player.CurrentLocation.LocationToEast);
             UpdateUI();
         }
 
         private void btnSouth_Click(object sender, System.EventArgs e)
         {
-            _player.MoveTo(_player.CurrentLocation.LocationToSouth);
+            MoveTo(_player.CurrentLocation.LocationToSouth);
             UpdateUI();
         }
 
         private void btnWest_Click(object sender, System.EventArgs e)
         {
-            _player.MoveTo(_player.CurrentLocation.LocationToWest);
+            MoveTo(_player.CurrentLocation.LocationToWest);
             UpdateUI();
         }
 
